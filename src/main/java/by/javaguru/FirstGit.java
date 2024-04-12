abf913f72fe01a132c2767d628309aabf36e07a1
serg0304 <warbdq@gmail.com>
2024-04-12 07:15:51 -0400

    del sout

--- src/main/java/by/javaguru/FirstGit.java
+++ src/main/java/by/javaguru/FirstGit.java
@@ -6,7 +6,6 @@ public class FirstGit
     {
         System.out.println("GIT DEVELOP!");
         System.out.println("LEARNING!");
-        System.out.println("It's terribly difficult");
         System.out.println("I'm happy!");
         System.out.println("!!!");
     }
