094b9aa9b185a8cd23ebe92db777689c24cee025
serg0304 <warbdq@gmail.com>
2024-04-12 07:13:05 -0400

    Add sout

--- src/main/java/by/javaguru/FirstGit.java
+++ src/main/java/by/javaguru/FirstGit.java
@@ -8,5 +8,6 @@ public class FirstGit
         System.out.println("LEARNING!");
         System.out.println("It's terribly difficult");
         System.out.println("I'm happy!");
+        System.out.println("!!!");
     }
 }
