bfa782d1e19d22f37e5630263158c39273000cf5
serg0304 <40315130+serg0304@users.noreply.github.com>
2024-04-12 06:30:29 -0400

    Update FirstGit.java
    Add '!' to sout

--- src/main/java/by/javaguru/FirstGit.java
+++ src/main/java/by/javaguru/FirstGit.java
@@ -1,14 +1,10 @@
 package by.javaguru;
 
-/**
- * Hello world!
- *
- */
 public class FirstGit
 {
     public static void main( String[] args )
     {
         System.out.println("GIT!");
-        System.out.println("LEARNING");
+        System.out.println("LEARNING!");
     }
 }
