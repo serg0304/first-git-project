727fa746fb5dc495a3332be0d448412521b4908f
serg0304 <warbdq@gmail.com>
2024-04-12 06:21:22 -0400

    Add class FirstGit

---
+++ src/main/java/by/javaguru/FirstGit.java
@@ -0,0 +1,13 @@
+package by.javaguru;
+
+/**
+ * Hello world!
+ *
+ */
+public class FirstGit
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
